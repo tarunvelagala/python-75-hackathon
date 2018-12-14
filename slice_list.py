f47ce64d7478f60708d90306d77409b105b772d8
Tarun Kumar <tarunvelagala80@gmail.com>
2018-12-15 01:52:27 +0530

    Program to Slice a list

---
+++ slice_list.py
@@ -0,0 +1,20 @@
+#indexing on list and tuple
+
+lst=[10,20,30,'ramu',-22.5]
+print(lst[0])
+print(lst[1])
+print(lst[2])
+print(len(lst))
+#slicing in list
+print(lst[0:3])
+print(lst[:])
+print(lst[0::2])
+print(lst[::-1])
+tpl=(1,2,3,4)
+print(tpl[0])
+print(len(tpl))
+print(tpl[:])
+print(tpl[3::-1])
+#repetition
+print(lst*2)
+print(tpl*2)
