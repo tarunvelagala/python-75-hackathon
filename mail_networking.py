6a55f57084bd5bf479f6c2d6e5d7d9bf38c70685
Tarun Kumar <tarunvelagala80@gmail.com>
2018-12-15 01:47:09 +0530

    Program to send mail using smtp

---
+++ mail_networking.py
@@ -0,0 +1,16 @@
+#Sending mail using Networking
+import smtplib
+from email.mime.text import MIMEText
+body = 'This is my text mail'
+msg = MIMEText(body)
+fromaddr = 'tarunvelagala80@gmail.com'
+toaddr = 'tsectioncse@gmail.com'
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Hello"
+server = smtplib.SMTP('smtp.gmaail.com', 587)
+server.starttls()
+server.login(fromaddr, "mypassword")
+server.send_message(msg)
+print('Mail sent......')
+server.quit()
