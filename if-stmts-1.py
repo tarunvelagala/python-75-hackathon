a5f98661f8331b053e828c338e9973af255da3d2
Tarun Kumar <tarunvelagala80@gmail.com>
2018-12-15 20:44:36 +0530

    If else program using bitwise opr

---
+++ if-stmts-1.py
@@ -0,0 +1,17 @@
+#to check even or odd using simple if
+n=int(input('enter a number'))
+
+if n & 1:
+    print('Even')
+else:
+    print('Odd')
+
+#to check even or odd
+
+n=int(input('enter a number'))
+if n==0:
+    print(n,'is zero')
+elif n%2==0:
+    print(n,'is even')
+else:
+    print(n,'is odd')
