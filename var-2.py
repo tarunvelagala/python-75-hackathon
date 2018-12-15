a9c48202ab5d750a69cf9a3cda36e534da15efd0
Tarun Kumar <tarunvelagala80@gmail.com>
2018-12-15 23:59:26 +0530

    defines the local and global scope of a variable

---
+++ var-2.py
@@ -0,0 +1,11 @@
+# defining local and global scope
+# global scope
+n = int(input("Enter a number"))
+
+def function():
+    # local scope
+    n = 10
+    print('Loacal Scope ---> n value =  ', n)
+
+function()
+print('Global Scope  ---> n value',n)
