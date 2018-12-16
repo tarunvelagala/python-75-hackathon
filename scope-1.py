1b3f19fc9cf0862b0dfe4a66ba52599393a8b4d1
Tarun Kumar <tarunvelagala80@gmail.com>
2018-12-16 17:10:34 +0530

    Defining the scope of a variable

--- scope-1.py
+++ scope-1.py
@@ -1,21 +1,23 @@
-a=0
+a = 0
 
-if a==0:
-    #this is global variable
-    b=1
+if a == 0:
+    # this is global variable
+    b = 1
+
 
 def fun(c):
-    #this is local variable
-    d=3
+    # this is local variable
+    d = 3
     print(c)
 
-#call function with value 7
+
+# call function with value 7
 fun(7)
 
-#a and b exist since these are global variables
+# a and b exist since these are global variables
 print(a)
 print(b)
 
-#c and d does not exist since local variables cannot be called outside
-#print(c)
-#print(d)
+# c and d does not exist since local variables cannot be called outside
+# print(c)
+# print(d)
